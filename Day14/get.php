8a618816de190367f731d1b02dd6e0eefe53654c
jaimil23 <125352516+jaimil23@users.noreply.github.com>
2023-07-14 10:29:46 +0530

    Day14

---
+++ Day14/get.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        hello <?php echo $_GET["name"]; ?><br>
+        Your Email address is: <?php echo $_GET["mail"]; ?>
+    </body>
+</html>
---
+++ Day14/post.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        hello <?php echo $_POST["name"]; ?><br>
+        Your Email address is: <?php echo $_POST["mail"]; ?>
+    </body>
+</html>
